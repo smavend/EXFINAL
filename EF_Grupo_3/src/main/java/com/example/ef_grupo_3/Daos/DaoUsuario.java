d6b6a689b2458fdf65161127e7ad153860af1ae7
amanr <amanrique068@gmail.com>
2022-11-29 13:24:25 -0500

    Servlet SERVICIOS

--- EF_Grupo_3/src/main/java/com/example/ef_grupo_3/Daos/DaoUsuario.java
+++ EF_Grupo_3/src/main/java/com/example/ef_grupo_3/Daos/DaoUsuario.java
@@ -3,7 +3,7 @@ package com.example.ef_grupo_3.Daos;
 import com.example.ef_grupo_3.Beans.Impresion3D;
 
 public class DaoUsuario extends DaoBase{
-    public float costoImpresion(Impresion3D impresion3D){
-        String sql = "SELECT * FROM impresion3D i inner join usuarios u on (i.)";
+    public float costoImpresion(int idImpresion, String correo){
+        String sql = "SELECT * FROM impresion3D i inner join usuarios u on (i.correo == u.correo) WHERE correo = ? and id = ";
     }
 }
---
+++ EF_Grupo_3/src/main/java/com/example/ef_grupo_3/Servlets/ServicesServlet.java
@@ -0,0 +1,20 @@
+package com.example.ef_grupo_3.Servlets;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "ServicesServlet", value = "/ServicesServlet")
+public class ServicesServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
