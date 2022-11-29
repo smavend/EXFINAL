15c02bc9781ea65adb95139de2e76191fb1b9d1e
jorgeluis03 <jorgel.dominguez03@gmail.com>
2022-11-29 14:12:59 -0500

    daos

--- EF_Grupo_3/src/main/java/com/example/ef_grupo_3/Beans/CorteLaser.java
+++ EF_Grupo_3/src/main/java/com/example/ef_grupo_3/Beans/CorteLaser.java
@@ -12,4 +12,6 @@ public class CorteLaser {
    Usuario usuario;
    double costo;
    double realizacion;
+
+
 }
--- EF_Grupo_3/src/main/java/com/example/ef_grupo_3/Daos/DaoServicios.java
+++ EF_Grupo_3/src/main/java/com/example/ef_grupo_3/Daos/DaoServicios.java
@@ -1,12 +1,20 @@
 package com.example.ef_grupo_3.Daos;
-
+import
+import com.example.ef_grupo_3.Beans.CorteLaser;
+import com.example.ef_grupo_3.Beans.Escaneo3D;
 import com.example.ef_grupo_3.Beans.Impresion3D;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class DaoServicios extends DaoBase{
 
     public ArrayList<Impresion3D> listarImpresion3D() {
         ArrayList<Impresion3D> listaImpresion3D = new ArrayList<>();
-        String sql = "SELECT * FROM arbitro";
+        String sql = "SELECT * FROM impresion3d";
 
         try (Connection connection = this.getConnection();
              Statement stm = connection.createStatement();
@@ -14,18 +22,61 @@ public class DaoServicios extends DaoBase{
 
 
             while(rs.next()){
-                Arbitro arbitro = new Arbitro();
-                arbitro.setIdArbitro(rs.getInt(1));
-                arbitro.setNombre(rs.getString(2));
-                arbitro.setPais(rs.getString(3));
+                Impresion3D impresion3D = new Impresion3D();
+                impresion3D.setFecha(rs.getString(9));
+                impresion3D.setCosto(rs.getDouble(11));
+                impresion3D.setRealizacion(rs.getDouble(12));
 
-                arbitros.add(arbitro);
+                listaImpresion3D.add(impresion3D);
             }
         } catch (SQLException throwables){
             throwables.printStackTrace();
         }
 
         return listaImpresion3D;
+    }
+
+    public ArrayList<CorteLaser> listarCorteLaser() {
+        ArrayList<CorteLaser> listaCorteLaser = new ArrayList<>();
+        String sql = "SELECT * FROM cortelaser";
+
+        try (Connection connection = this.getConnection();
+             Statement stm = connection.createStatement();
+             ResultSet rs = stm.executeQuery(sql)) {
+
+            while(rs.next()){
+                CorteLaser corteLaser = new CorteLaser();
+
+
+                listaCorteLaser.add(corteLaser);
+            }
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        return listaCorteLaser;
+    }
+    public ArrayList<Escaneo3D> listarEscaneo3D() {
+        ArrayList<Escaneo3D> listaEscaneo3D = new ArrayList<>();
+        String sql = "SELECT * FROM escaneo3d";
+
+        try (Connection connection = this.getConnection();
+             Statement stm = connection.createStatement();
+             ResultSet rs = stm.executeQuery(sql)) {
+
+
+            while(rs.next()){
+                Escaneo3D escaneo3D = new Escaneo3D();
+
+
+
+                listaEscaneo3D.add(escaneo3D);
+            }
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        return listaEscaneo3D;
     }
 
 
